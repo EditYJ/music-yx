783dcbc881f0ec057d5adaf45a70904a5931b10b
EditYJ <158392613@qq.com>
2020-01-24 16:19:52 +0800

    列表项点击后标记当前选中列表项

--- miniprogram/components/musicSheet/musicSheet.js
+++ miniprogram/components/musicSheet/musicSheet.js
@@ -18,12 +18,17 @@ Component({
    * 组件的初始数据
    */
   data: {
+    currectMusicId: -1
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
-  }
+    handleClickMusicContent(event) {
+      this.setData({
+        currectMusicId: event.currentTarget.dataset.musicId
+      })
+    }
+  },
 })

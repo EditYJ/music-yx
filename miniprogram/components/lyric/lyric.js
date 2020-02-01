98644140b1548f5ed5f315ce96552765d3896339
EditYJ <158392613@qq.com>
2020-02-01 12:40:50 +0800

    歌词解析

--- miniprogram/components/lyric/lyric.js
+++ miniprogram/components/lyric/lyric.js
@@ -10,7 +10,9 @@ Component({
 
   observers: {
     lyric(lrc) {
-      console.log(lrc)
+      this.setData({
+        lrc:[...this.parseLrc(lrc)]
+      })
     }
   },
 
@@ -18,7 +20,7 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    lrc: []
   },
 
   /**
@@ -27,6 +29,24 @@ Component({
   methods: {
     changeLyricState() {
       this.triggerEvent('changeLyricState')
+    },
+    // 解析歌词
+    // return {
+    //  time,
+    //  text
+    // }
+    parseLrc(lrc) {
+      const lrcReg = /\[(\d{2,}):(\d{2})(?:\.(\d{2,3}))?]/g
+      const lines = lrc.split('\n')
+      const parsedLrc = lines.map(line => {
+        const lineDetail = line.split(lrcReg)
+        const time = parseInt(lineDetail[1]) * 60 + parseInt(lineDetail[2]) + parseInt(lineDetail[3]) / 1000
+        return {
+          time,
+          text: lineDetail[4]
+        }
+      })
+      return parsedLrc
     }
   }
 })

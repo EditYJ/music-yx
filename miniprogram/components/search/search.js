56df777f61d183433f5749e52a4dd2814af99f5d
EditYJ <158392613@qq.com>
2020-02-04 13:19:26 +0800

    添加发现评论页面//搜索组件样式完成//准备添加底部模态框组件

---
+++ miniprogram/components/search/search.js
@@ -0,0 +1,31 @@
+// components/search/search.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    placeholder: {
+      type: String,
+      value: '请输入内容...'
+    }
+  },
+
+  externalClasses: [
+    'iconfont-class',
+    'search-icon'
+  ],
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})

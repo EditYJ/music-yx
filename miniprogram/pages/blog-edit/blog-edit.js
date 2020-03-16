143853f5c2e79f945e9775ac5383c7027907a0ae
EditYJ <158392613@qq.com>
2020-03-16 10:09:39 +0800

    发布页面基本布局完成，监听键盘事件，获取键盘高度，自动拉起底边栏

--- miniprogram/pages/blog-edit/blog-edit.js
+++ miniprogram/pages/blog-edit/blog-edit.js
@@ -1,11 +1,13 @@
 // pages/blog-edit/blog-edit.js
+const MAX_WORDS_LENGTH = 140
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    wordsNum: 0,
+    bottom: 0
   },
 
   /**
@@ -62,5 +64,29 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  //////////////
+  // 处理输入事件
+  handleInput(e) {
+    let length = e.detail.value.length
+    if (length >= MAX_WORDS_LENGTH) {
+      length = `最大字数为${MAX_WORDS_LENGTH}`
+    }
+    this.setData({
+      wordsNum: length
+    })
+  },
+  // 处理获取焦点事件
+  handleFocus(e) {
+    this.setData({
+      bottom: e.detail.height
+    })
+  },
+  // 处理失去焦点事件
+  handleBlur(e) {
+    this.setData({
+      bottom: 0
+    })
   }
 })

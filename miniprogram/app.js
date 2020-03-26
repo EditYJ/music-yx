a8c78169575a328f46fcad466d988fb5e3a8870d
EditYJ <158392613@qq.com>
2020-03-26 09:25:27 +0800

    添加检查更新逻辑

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,7 @@
 //app.js
 App({
   onLaunch: function () {
-
+    this.checkAppUpdate()
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -28,6 +28,8 @@ App({
   getCurrentMusicId() {
     return this.globalData.currentMusicId
   },
+
+  // 获取用户openid
   getOpenId() {
     wx.cloud.callFunction({
       name: 'login',
@@ -39,6 +41,24 @@ App({
       if (wx.getStorageSync(openid) == '') {
         wx.setStorageSync(openid, [])
       }
+    })
+  },
+
+  // 检查更新函数
+  checkAppUpdate() {
+    const updateManager = wx.getUpdateManager()
+
+    updateManager.onUpdateReady(() => {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function (res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
     })
   }
 })
